2af3b454ff6b1ee21da9a3ec90c4deace1c4f5d6
Victor Klimov <4klimov@gmail.com>
2016-12-02 01:43:42 +0300

    Python lesson 3 First part of hard

--- python/3/home_work/hw03_hard.py
+++ python/3/home_work/hw03_hard.py
@@ -18,6 +18,65 @@ import os
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
 
+class Person:
+    def __init__(self, full_name, salary, job, time_rate):
+        self.full_name = full_name
+        self.salary = int(salary)
+        self.job = job
+        self.time_rate = int(time_rate)
+        self.hours = None
+        self.real_salary = None
+    def add_hours(self, real_hours):
+        self.real_hours = int(real_hours)
+    def show_data(self):
+        return "{0} {1} {2} {3} {4} {5}".format(self.full_name, self.job,
+                                                self.salary, self.time_rate, self.real_hours, self.real_salary)
+    def get_salary(self):
+        self.salary_count()
+        return self.real_salary
+    def salary_count(self):
+        addition = 0
+        if self.real_hours > self.time_rate:
+            addition = (self.salary / self.time_rate) * (self.real_hours - self.time_rate)
+        self.real_salary = round((self.salary / self.time_rate) * self.real_hours + addition, 0)
+
+
+def salary_counter():
+
+    # Getting data from files
+    cwd = os.getcwd()
+    dir = os.path.join(cwd, "data")
+    path_workers = os.path.join(dir, "workers")
+    path_hours_of = os.path.join(dir, "hours_of")
+    with open(path_workers, "r") as file:
+       data = file.read().split("\n")[1:]
+    with open(path_hours_of, "r") as file:
+       hours = file.read().split("\n")[1:]
+
+    # Uploading data
+    staff = {}
+    for line in data:
+        line = line.split()
+        full_name = " ".join(line[:2])
+        staff[full_name] = Person(full_name, line[2], line[3], line[4])
+
+    for line in hours:
+        line = line.split()
+        full_name = " ".join(line[:2])
+        staff[full_name].add_hours(line[2])
+
+    for i in staff:
+        print(staff[i].show_data())
+    print("--------------------------------------------------")
+    for i in staff:
+        staff[i].salary_count()
+
+    for i in staff:
+        print(staff[i].show_data())
+
+
+salary_counter()
+
 # Задание-3:
 # Дан файл ("data/fruits") со списком фруктов.
 # Записать в новые файлы все фрукты, начинающиеся с определенной буквы.
@@ -55,4 +114,4 @@ def sort_fruits():
                 for item in fruits_dict[letter]:
                     file.write(item+"\n")
 
-sort_fruits()
+# sort_fruits()
