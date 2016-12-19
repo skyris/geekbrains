2e36dd9bf1caf768e929643f7e55496e0a5f646a
Victor Klimov <4klimov@gmail.com>
2016-12-20 02:10:01 +0300

    Parsing script

---
+++ python/8/home_work/city_to_coordinate.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    Script for html parsing static webpage: erichware.info/sposob/geograf/geogorod.htm.
+    Creates nested dict in format:
+    {Country: {
+            Some_city: [latitude, longitude],
+            other_city: [latitude, longitude]
+            }
+    }
+"""
+
+import collections
+import re
+
+
+__author__ = "Victor Klimov"
+__copyright__ = "Creative Commons License"
+
+
+city_pattern = re.compile(r"<option value=\"(.+)\">(.+)")
+country_patter = re.compile(r"<optgroup label=\"(.+)\">")
+
+d = collections.defaultdict(dict)
+saved_country = ""
+flag = False
+path = "./coordinates/raw_cities"
+with open(path, "r") as f:
+    for line in f:
+        country  = country_patter.findall(line)
+        city = city_pattern.findall(line)
+        print(line)
+        if country:
+            saved_country = country[0]
+        if saved_country and city and not country :
+            coord = city[0][0].replace("+", "").split(",")
+            if len(coord) != 2:
+                continue
+            print("city",city[0][0])
+            print(coord)
+            d[saved_country][city[0][1]] = coord
+            print(d)
+            print(saved_country)
+            print(city[0])
+
+
+print(d["Россия"])
