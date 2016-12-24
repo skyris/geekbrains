e0d8c12052373892392fd54b8c42b0def4f7229c
Victor Klimov <4klimov@gmail.com>
2016-12-24 03:24:49 +0300

    Fix some methods in class FakeEmail

--- python/8/home_work/fake_email.py
+++ python/8/home_work/fake_email.py
@@ -16,11 +16,12 @@ __copyright__ = "Creative Commons License"
 email = "Grem1962@fleckens.hu"
 
 class FakeEmail:
-    email_sites = ("@armyspy.com", "@cuvox.de", "@dayrep.com", "@einrot.com", "@fleckens.hu",
+    def __init__(self):
+        self.email_sites = ("@armyspy.com", "@cuvox.de", "@dayrep.com", "@einrot.com", "@fleckens.hu",
                    "@gustr.com", "@jourrapide.com", "@rhyta.com", "@superrito.com", "@teleworm.us")
-    email_url_template = "http://www.fakemailgenerator.com/#/{}/{}/"
+        self.url_template = "http://www.fakemailgenerator.com/#/{}/{}/"
 
-    def get_new_email_address(self):
+    def get_new_address(self):
         base_url = "http://www.fakemailgenerator.com/"
         g = Grab()
         g.setup(connect_timeout=20, timeout=20)
@@ -28,19 +29,31 @@ class FakeEmail:
         if g.response.code == 200:
             email_name = g.doc('//*[@id="home-email"]/@value').text()
             email_site = g.doc('//*[@id="domain"]').text()
-            self.email = email_name + email_site
-            self.full_http = self.url_template.format(email_name, email_site.strip("@"))
-            return self.full_http
+            self.full_email = email_name, email_site
 
-    def generate_new_email_address(self):
-        new_email_site = random.choice(self.email_sites)
+    def generate_address(self, email_name):
+        self.full_email = email_name, random.choice(self.email_sites)
 
-    def check_new_emails(self, url):
+    @property
+    def email(self):
+        return "{}{}".format(self.full_email[0], self.full_email[1])
+
+    @property
+    def email_url(self):
+        return self.url_template.format(self.full_email[0], self.full_email[1].strip("@"))
+
+    def check_inbox(self):
         g = Grab()
         g.setup(connect_timeout=20, timeout=20)
-        g.go(url)
+        g.go(self.email_url)
         if g.response.code == 200:
             print(g.response.body)
+
+
+
+fake_email = FakeEmail()
+fake_email.get_new_address()
+fake_email.check_inbox()
 
 """
 
