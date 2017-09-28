2f6e96c43c39982e700f5885bc53f2649b29ad6a
Victor Klimov <4klimov@gmail.com>
2017-09-28 03:10:05 +0300

    Add shuffle on server. Add ajax request on client

--- javascript2/2/gallery/index.js
+++ javascript2/2/gallery/index.js
@@ -1,16 +1,34 @@
 const fs = require('fs')
-const express = require('express')
-
 var pictures_data = fs.readFileSync('public/pictures.json', 'utf8')
+var dataObj = JSON.parse(pictures_data)
+
+// Fisher-Yates shuffle for our array
+function shuffle (array) {
+    var i = 0
+        , j = 0
+        , temp = null
+
+    for (i = array.length - 1; i > 0; i -= 1) {
+        j = Math.floor(Math.random() * (i + 1))
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+}
+
+shuffle(dataObj)
+pictures_data = JSON.stringify(dataObj)
+
+const express = require('express')
 const app = express()
-app.use(express.static('public'));
 const port = 5000
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
   response.sendfile('index.html')
 })
 
-
+// Router for ajax
 app.get('/json/', (request, response) => {
   response.send(pictures_data)
 })
--- javascript2/2/gallery/public/script.js
+++ javascript2/2/gallery/public/script.js
@@ -1,11 +1,9 @@
-console.log("hello");
 
 // Get the modal
 var modal = document.getElementById('myModal');
 var modalImg = document.getElementById("modalImg ");
 var captionText = document.getElementById("caption");
 function modalOnClick(){
-    console.log(this);
     modal.style.display = "block";
     modalImg.src = this.getAttribute("pic");
     captionText.innerHTML = this.alt;
@@ -47,17 +45,30 @@ Picture.prototype.makeElement = function() {
     img.setAttribute("pic", "images/" + this.path)
     return img;
 };
-var from_server = '[ { "path": "img001_1200_800.jpeg", "thumbPath": "img001_300_200.jpeg", "desc": "" }, { "path": "img002_1200_800.jpeg", "thumbPath": "img002_300_200.jpeg", "desc": "" }, { "path": "img003_1200_800.jpeg", "thumbPath": "img003_300_200.jpeg", "desc": "" }, { "path": "img004_1200_800.jpeg", "thumbPath": "img004_300_200.jpeg", "desc": "" }, { "path": "img005_1200_800.jpeg", "thumbPath": "img005_300_200.jpeg", "desc": "" }, { "path": "img006_1200_800.jpeg", "thumbPath": "img006_300_200.jpeg", "desc": "" }, { "path": "img007_1200_800.jpeg", "thumbPath": "img007_300_200.jpeg", "desc": "" }, { "path": "img008_1200_800.jpeg", "thumbPath": "img008_300_200.jpeg", "desc": "" }, { "path": "img009_1200_800.jpeg", "thumbPath": "img009_300_200.jpeg", "desc": "" }, { "path": "img010_1200_800.jpeg", "thumbPath": "img010_300_200.jpeg", "desc": "" }, { "path": "img011_1200_800.jpeg", "thumbPath": "img011_300_200.jpeg", "desc": "" }, { "path": "img012_1200_800.jpeg", "thumbPath": "img012_300_200.jpeg", "desc": "" }, { "path": "img013_1200_800.jpeg", "thumbPath": "img013_300_200.jpeg", "desc": "" }, { "path": "img014_1200_800.jpeg", "thumbPath": "img014_300_200.jpeg", "desc": "" }, { "path": "img015_1200_800.jpeg", "thumbPath": "img015_300_200.jpeg", "desc": "" }, { "path": "img016_1200_800.jpeg", "thumbPath": "img016_300_200.jpeg", "desc": "" }, { "path": "img017_1200_800.jpeg", "thumbPath": "img017_300_200.jpeg", "desc": "" }, { "path": "img018_1200_800.jpeg", "thumbPath": "img018_300_200.jpeg", "desc": "" }, { "path": "img019_1200_800.jpeg", "thumbPath": "img019_300_200.jpeg", "desc": "" }, { "path": "img020_1200_800.jpeg", "thumbPath": "img020_300_200.jpeg", "desc": "" }, { "path": "img021_1200_800.jpeg", "thumbPath": "img021_300_200.jpeg", "desc": "" } ]';
-var picsData = JSON.parse(from_server);
+
 var galleryFrame = document.getElementById("galleryFrame");
 var picturesArray = [];
-for(var currPicData of picsData) {
-    var pictureElement = new Picture(currPicData);
-    var elem = pictureElement.makeElement();
-    elem.onclick = modalOnClick;
-    galleryFrame.appendChild(elem);
-    picturesArray.push(elem);
-}
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "/json/", true);
+xhr.timeout = 15000;
+xhr.ontimeout = function () {
+    console.log("Timeout expired!");
+};
+xhr.send(null);
+var dataFromServer;
+xhr.onreadystatechange = function () {
+   if(xhr.readyState == xhr.DONE && xhr.status == 200) {
+     dataFromServer = JSON.parse(this.responseText);
+     for(var currData of dataFromServer ) {
+         var pictureElement = new Picture(currData);
+         var elem = pictureElement.makeElement();
+         elem.onclick = modalOnClick;
+         galleryFrame.appendChild(elem);
+         picturesArray.push(elem);
+     }
+   }
+};
 
 
 
