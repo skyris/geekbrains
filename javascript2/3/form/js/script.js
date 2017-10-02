e46ca5089a1bc0eac040dddc775982d039822b36
Victor Klimov <4klimov@gmail.com>
2017-10-02 20:41:56 +0300

    Refactoring

--- javascript2/3/form/js/script.js
+++ javascript2/3/form/js/script.js
@@ -1,82 +1,44 @@
 function validateForm() {
     var flagSubmit = true;
+    validateItem("name", isValidName, "Please enter at least two characters");
+    validateItem("phone", isValidPhone, "Please enter valid phone number");
+    validateItem("email", isValidEmail, "Please enter valid email");
+    validateItem("message", isValidMessage, "Please enter your message");
+
     var form = document.getElementById("form");
-    var name = document.getElementById("name");
-    if (isValidName(name.value)) {
-        name.style.boxShadow = "0 0 10px #181";
-        name.style.border = "1px solid #060";
-        var nameFeedback = document.getElementById("name-feedback");
-        nameFeedback.innerText = "";
-    } else {
-        name.style.boxShadow = "0 0 10px #811";
-        name.style.border = "1px solid #600";
-        var nameFeedback = document.getElementById("name-feedback");
-        nameFeedback.innerText = "Please enter at least two characters";
-        flagSubmit = false;
-    }
-
-    var phone = document.getElementById("phone");
-    if (isValidPhone(phone.value)) {
-        phone.style.boxShadow = "0 0 10px #181";
-        phone.style.border = "1px solid #060";
-        var phoneFeedback = document.getElementById("phone-feedback");
-        phoneFeedback.innerText = "";
-    } else {
-        phone.style.boxShadow = "0 0 10px #811";
-        phone.style.border = "1px solid #600";
-        var phoneFeedback = document.getElementById("phone-feedback");
-        phoneFeedback.innerText = "Please enter valid phone number";
-        flagSubmit = false;
-    }
-
-    var email = document.getElementById("email");
-    if (isValidEmail(email.value)) {
-        email.style.boxShadow = "0 0 10px #181";
-        email.style.border = "1px solid #060";
-        var emailFeedback = document.getElementById("phone-feedback");
-        emailFeedback.innerText = "";
-    } else {
-        email.style.boxShadow = "0 0 10px #811";
-        email.style.border = "1px solid #600";
-        var emailFeedback = document.getElementById("email-feedback");
-        emailFeedback.innerText = "Please enter valid email";
-        flagSubmit = false;
-    }
-    var message = document.getElementById("message");
-    if (isValidMessage(message.value)) {
-        message.style.boxShadow = "0 0 10px #181";
-        message.style.border = "1px solid #060";
-        var messageFeedback = document.getElementById("message-feedback");
-        messageFeedback.innerText = "";
-    } else {
-        message.style.boxShadow = "0 0 10px #811";
-        message.style.border = "1px solid #600";
-        var messageFeedback = document.getElementById("message-feedback");
-        messageFeedback.innerText = "Please some message";
-        flagSubmit = false;
-    }
-
     if(flagSubmit) {
         form.submit();
     }
 }
 
-
-function isValidName(name) {
-   return /^[A-Za-zА-Яа-яёЁ]+$/.test(name);
+function validateItem(id, validateFunction, feedbackMessage) {
+    var element = document.getElementById(id);
+    var elementFeedback = document.getElementById(id + "-feedback");
+    if (validateFunction(element.value)) {
+        element.style.boxShadow = "0 0 10px #181";
+        element.style.border = "1px solid #060";
+        elementFeedback.innerText = "";
+    } else {
+        element.style.boxShadow = "0 0 10px #811";
+        element.style.border = "1px solid #600";
+        elementFeedback.innerText = feedbackMessage;
+        flagSubmit = false;
+    }
 }
 
+function isValidName(name) {
+    return /^[A-Za-zА-Яа-яёЁ]+$/.test(name);
+}
 
 function isValidPhone(phone) {
     return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/.test(phone);
 }
-
 
 function isValidEmail(email) {
     return /[\w.-]+@\w+?\.[A-Za-z]{2,6}/.test(email);
 }
 
 function isValidMessage(text) {
-   return text.length > 0;
+    return text.length > 0;
 }
 
